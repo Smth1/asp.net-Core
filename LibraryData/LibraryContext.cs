e5eab14b8cf7ae5771fa820d75ea5f01cfe607cf
Roman <2016roman00@gmail.com>
2018-08-12 23:02:13 +0300

    Creating Database

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+using LibraryData;
 
 namespace WebApplication1
 {
@@ -33,6 +37,9 @@ namespace WebApplication1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<LibraryContext>(options
+                => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
